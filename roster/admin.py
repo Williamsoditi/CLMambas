a9a0a6bfc4a202e5c7100d2a99510751b52f15b1
Williamsoditi <williamsoditi99@gmail.com>
2025-12-04 16:56:45 +0300

    Fix : Bugs resolution

--- roster/admin.py
+++ roster/admin.py
@@ -4,16 +4,16 @@ from .models import Player
 # Register your models here.
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'jersey_number', 'position' , 'height')
+    list_display = ('name', 'jersey_number', 'position' , 'height_ft', 'weight_kgs')
     search_fields = ('name', 'jersey_number', 'position')
     list_filter = ('position',)
     ordering = ('jersey_number',)
     fieldsets = (
-        (None, {
+        ('Athlete Data', {
             'fields': ('name', 'jersey_number', 'position')
         }),
         ('Physical Attributes', {
-            'fields': ('height', 'weight'),
+            'fields': ('height_ft', 'weight_kgs'),
         }),
         ('Image', {
             'fields': ('image',)
--- roster/models.py
+++ roster/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Player(models.Model):
     name = models.CharField(max_length=100, default='New Player')
-    height= models.DecimalField(max_digits=1, decimal_places=1, max_length=5, blank=True, null=
+    height_ft= models.DecimalField(max_digits=3, decimal_places=1, max_length=5, blank=True, null=
     True)  # height in feet
     position = models.CharField(max_length=50, blank=True, null=True, choices=[
         ('Point Guard', 'Point Guard'),
@@ -16,9 +16,9 @@ class Player(models.Model):
     ])
     jersey_number = models.PositiveIntegerField(unique=True)
     image = CloudinaryField('Player Photo', blank=True, null=True)
-
+    weight_kgs = models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)  # weight in kgs
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - #{self.number} ({self.position})"
+        return f"{self.name} - #{self.jersey_number} ({self.position})"
 
     class Meta:
         verbose_name_plural = 'Players'
